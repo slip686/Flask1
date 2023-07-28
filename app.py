d6b73312da54dac70e8945c127c95d27d024ef5c
Zvonkin Kirill <slip686@gmail.com>
2023-07-28 16:23:03 +0300

    ORM added

--- app.py
+++ app.py
@@ -1,148 +1,63 @@
-from flask import Flask
-from flask import json
-from flask import request
-import random
-import sqlite3
-from markupsafe import escape
+from flask import Flask, abort, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from pathlib import Path
 
-json.provider.DefaultJSONProvider.ensure_ascii = False
+BASE_DIR = Path(__file__).parent
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{BASE_DIR / 'main.db'}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.app_context().push()
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
-def tuple_to_dict(quote: tuple):
-    keys = ["id", "author", "text"]
-    return dict(zip(keys, quote))
+class AuthorModel(db.Model):
+   id = db.Column(db.Integer, primary_key=True)
+   name = db.Column(db.String(32), unique=True)
+   quotes = db.relationship('QuoteModel', backref='author', lazy='dynamic')
+
+   def __init__(self, name):
+       self.name = name
 
 
-def get_all_quotes_query():
-    select_all = "SELECT * FROM quotes"
-    return select_all
+class QuoteModel(db.Model):
+   id = db.Column(db.Integer, primary_key=True)
+   author_id = db.Column(db.Integer, db.ForeignKey(AuthorModel.id))
+   text = db.Column(db.String(255), unique=False)
+
+   def __init__(self, author, text):
+       self.author_id = author.id
+       self.text = text
 
 
-def get_quote_by_id_query(id):
-    select_by_id = "SELECT * FROM quotes WHERE id={}".format(id)
-    return select_by_id
-
-
-def add_new_quote_query(author, text):
-    add_new_quote = """INSERT INTO quotes (author, text) VALUES ("{}", "{}")""".format(author, text)
-    return add_new_quote
-
-
-def db_query_exec(query: str, select_mode=None):
-    result = None
-    connection = sqlite3.connect("test.db")
-    cursor = connection.cursor()
-    cursor.execute(query)
-    print(query)
-    if select_mode:
-        if select_mode == 'fetchall':
-            result = cursor.fetchall()
-        if select_mode == 'fetchone':
-            result = cursor.fetchone()
-    else:
-        connection.commit()
-    cursor.close()
-    connection.close()
-    if result:
-        return list(map(tuple_to_dict, result))
-
-
-
-
+# dict --> JSON - сериализация
+# JSON --> dict - десериализация
+# Object --> dict --> JSON
 @app.route("/quotes")
-def quotes_list():
-    result = db_query_exec(get_all_quotes_query(), select_mode='fetchall')
-    return result
+def get_quotes():
+    quotes: list[QuoteModel] = QuoteModel.query.all()
+    quotes_dict: list[dict] = []
+    for quote in quotes:
+        quotes_dict.append(quote.to_dict())
+    return quotes_dict
 
 
 @app.route("/quotes/<int:quote_id>")
 def get_quote_by_id(quote_id):
-    result = db_query_exec(get_quote_by_id_query(quote_id), select_mode='fetchall')
-    print(get_quote_by_id_query(quote_id))
-    if result:
-        return result
-    else:
-        return f"Quote with id={quote_id} not found", 404
+    quote = QuoteModel.query.get(quote_id)
+    if quote is None:
+        return "Not found", 404
+    return quote.to_dict()
 
 
-# @app.route("/quotes/count")
-# def get_quotes_count():
-#     return {"count": f"{len(quotes)}"}
-
-
-# @app.route("/quotes/random")
-# def get_random_quote():
-#     return random.choice(quotes)
-
-
-@app.route("/quotes", methods=['POST'])
+@app.route("/quotes", methods=["POST"])
 def create_quote():
-    data = request.json
-    db_query_exec(add_new_quote_query(author=data.get("author"), text=data.get("text")))
-    print("data = ", data)
-    return data, 201
-
-
-# @app.route("/quotes/<quote_id>", methods=["PUT"])
-# def edit_quote(quote_id):
-#     new_data = request.json
-#     for quote in quotes:
-#         if quote_id == quote["id"]:
-#             if new_data.get("text"):
-#                 quote["text"] = new_data["text"]
-#             if new_data.get("author"):
-#                 quote["author"] = new_data["author"]
-#             if new_data.get("rating"):
-#                 if int(new_data["rating"]) <= 5:
-#                     quote["rating"] = int(new_data["rating"])
-#             return quote, 201
-#     return f"Quote with id={quote_id} not found", 404
-#
-#
-# @app.route("/quotes/<quote_id>", methods=["DELETE"])
-# def delete_quote(quote_id):
-#     for quote in quotes:
-#         if int(quote_id) == quote["id"]:
-#             quotes.remove(quote)
-#             return f"Quote with id {quote_id} is deleted.", 200
-#     return f"Quote with id={quote_id} not found", 404
-#
-#
-# @app.route("/quotes/filter")
-# def get_quote_with_filter():
-#     args = request.args
-#     author = args.get("author")
-#     rating = args.get("rating")
-#     result = []
-#
-#     if args.get("rating_range"):
-#         rating_range = args["rating_range"].split(",")
-#         for quote in quotes:
-#             if quote["rating"] in range(int(rating_range[0]), int(rating_range[1]) + 1):
-#                 result.append(quote)
-#     elif args.get("id_range"):
-#         id_range = args["id_range"].split(",")
-#         for quote in quotes:
-#             if quote["id"] in range(int(id_range[0]), int(id_range[1]) + 1):
-#                 result.append(quote)
-#     else:
-#         if None not in (author, rating):
-#             for quote in quotes:
-#                 if quote["author"] == author and quote["rating"] == int(rating):
-#                     result.append(quote)
-#
-#         elif author is not None:
-#             for quote in quotes:
-#                 if quote["author"] == author:
-#                     result.append(quote)
-#
-#         elif rating is not None:
-#             for quote in quotes:
-#                 if quote["rating"] == int(rating):
-#                     result.append(quote)
-#     if result:
-#         return result, 200
-#     else:
-#         return f"Quotes not found", 404
+    new_quote = request.json
+    quote = QuoteModel(**new_quote)
+    db.session.add(quote)
+    db.session.commit()
+    return quote.to_dict(), 201
