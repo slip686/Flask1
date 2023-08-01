61d1217da81166f999f6b50fd4d7358b2f6bf553
Zvonkin Kirill <slip686@gmail.com>
2023-08-01 16:12:54 +0300

    db changed to local postgres

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).parent
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://slip686:ddtlbnt yjdsq@host.docker.internal:5432/quotes"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://slip686:ddtlbnt yjdsq@srv-captain--exon-db:5432/exon"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.app_context().push()
 
